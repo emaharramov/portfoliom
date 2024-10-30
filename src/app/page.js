fe8fbcd4d9739c4eed6c8bdb44fc4d02ea6188a6
Emil Maharramov <emilmaharramov@Emil-MacBook-Air.local>
2024-10-30 15:17:56 +0400

    added social icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { SocialIcon } from "react-social-icons";
+
 export default function Home() {
   
   return (
@@ -5,6 +7,11 @@ export default function Home() {
         <div className="spin">
           <h1>Our Page is Under Construction</h1>
           <p>Please check back soon!</p>
+          <div className="flex justify-center items-center gap-x-3">
+            <SocialIcon url="https://linkedin.com/in/emilmaharramov" />
+            <SocialIcon network="whatsapp" href="https://wa.me/994502070180"  />
+            <SocialIcon network="instagram" href="https://instagram.com/emilmaharramovv" />
+          </div>
         </div>
       </div>
   );
